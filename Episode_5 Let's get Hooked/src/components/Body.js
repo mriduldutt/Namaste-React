8a1b0a3d1d209f0234f48ce72869e0b1babc78c2
mriduldutt <mirdul12dutt@gmail.com>
2024-04-04 00:18:30 +0530

    "Episode_5"

--- Episode_5 Let's get Hooked/src/components/Body.js
+++ Episode_5 Let's get Hooked/src/components/Body.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import RestaurantCard from "./RestaurantCard";
-// import resList from "../utils/mockData";
+import resList from "../utils/mockData";
 
 
 
@@ -8,70 +8,7 @@ import RestaurantCard from "./RestaurantCard";
 const Body = () => {
   
   // Local State Variable - Super Powerful Variable
-   const [listofRestraunts, setListofRestraunts] = useState([     
- {
-    "info": {
-      "id": "435682",
-      "name": "Pizza Hut",
-      "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
-      "locality": "Adarsh Bhawan Chbs",
-      "areaName": "Punjabi Bagh",
-      "costForTwo": "₹350 for two",
-      "cuisines": [
-        "Pizzas"
-      ],
-      "avgRating": 4.4,
-      "parentId": "721",
-      "avgRatingString": "4.4",
-      "totalRatingsString": "1K+",
-      "sla": {
-        "deliveryTime": 33,
-      },
-    },
-  },
-  {
-    "info": {
-      "id": "435681",
-      "name": "Dominos",
-      "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
-      "locality": "Adarsh Bhawan Chbs",
-      "areaName": "Punjabi Bagh",
-      "costForTwo": "₹350 for two",
-      "cuisines": [
-        "Pizzas"
-      ],
-      "avgRating": 3.8,
-      "parentId": "721",
-      "avgRatingString": "3.8",
-      "totalRatingsString": "1K+",
-      "sla": {
-        "deliveryTime": 33,
-      },
-    },
-
-  },
-  {
-    "info": {
-      "id": "435683",
-      "name": "MCD",
-      "cloudinaryImageId": "2b4f62d606d1b2bfba9ba9e5386fabb7",
-      "locality": "Adarsh Bhawan Chbs",
-      "areaName": "Punjabi Bagh",
-      "costForTwo": "₹350 for two",
-      "cuisines": [
-        "Pizzas"
-      ],
-      "avgRating": 4.1,
-      "parentId": "721",
-      "avgRatingString": "4.1",
-      "totalRatingsString": "1K+",
-      "sla": {
-        "deliveryTime": 33,
-      },
-    },
-
-  }
-]);
+   const [listofRestraunts, setListofRestraunts] = useState(resList);
 
   // // Normal JS Variable
   // let listofRestrauntsjs = [
@@ -146,7 +83,7 @@ const Body = () => {
           <button className="filter-btn" onClick={() => {
             //Filter Logic here
             const filteredList = listofRestraunts.filter((resData)=>{
-                return resData.info.avgRating > 4;
+                return resData.info.avgRating > 4.3;
             })
 
             setListofRestraunts(filteredList)
