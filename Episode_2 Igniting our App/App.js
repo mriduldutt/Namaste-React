98ef6d50f8ea910344408021e937c2ded4bd96d2
mriduldutt <mirdul12dutt@gmail.com>
2024-04-24 01:00:58 +0530

    Episode_2

--- Episode_2 Igniting our App/App.js
+++ Episode_2 Igniting our App/App.js
@@ -11,7 +11,7 @@ const parent = React.createElement("div", { id: "parent" }, [
       React.createElement("h1", {}, "I am h1 tag"),
       React.createElement("h2", {}, "I am h2 tag"),
     ]),
-  ]);
+  ]); 
   
  
   const root = ReactDOM.createRoot(document.getElementById("root"));
