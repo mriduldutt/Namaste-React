3ade7fd0b4a1fca5dc90e53fe821daef72d567c1
mriduldutt <mirdul12dutt@gmail.com>
2024-04-25 11:58:09 +0530

    Episode

--- Episode_13 Time for test/src/components/sum.js
+++
@@ -1,5 +0,0 @@
-export const sum = (a,b) => {
-    let c = a + b;
-    return c;
-}
-
--- Episode_13 Time for test/src/utils/userContext.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const userContext = React.createContext({
-    loggedInUser: "Default User",
-}) // null is the default value
-
-export default userContext;
