953516eca9eec0dbc1c4c8c0fba976e01c47019a
mriduldutt <mirdul12dutt@gmail.com>
2024-05-05 16:01:42 +0530

    Episode_12

--- Episode_12 Lets Build our Store/src/utils/cartReduxStoreSlice.js
+++ Episode_12 Lets Build our Store/src/utils/cartReduxStoreSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, nanoid,current } from "@reduxjs/toolkit";
 
 
-const cartReduxStoreSlice = createSlice({
+const cartReduxSlice = createSlice({
     name : 'cart',
     initialState : {
         items: JSON.parse(localStorage.getItem("cart")) || [],
@@ -51,7 +51,7 @@ const cartReduxStoreSlice = createSlice({
 
 export const {addItem, removeItem, clearCart} = cartReduxStoreSlice.actions;
 
-export default cartReduxStoreSlice.reducer;
+export default cartReduxSlice.reducer;
 
 
 
