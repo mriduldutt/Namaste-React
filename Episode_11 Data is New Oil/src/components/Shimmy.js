bebc960b6f8813db03afccab28e12db8bd729e1e
Mridul Dutt <mirdul12dutt@gmail.com>
2025-06-23 18:25:39 +0530

    added

---
+++ Episode_11 Data is New Oil/src/components/Shimmy.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Shimmy = () => {
+  return (
+    <div>
+      <div className="m-4 p-4 w-3/12 h-3/12 rounded-lg bg-gray-200 hover:bg-gray-200">
+      <div className="m-4 p-4 w-3/12 h-3/12 rounded-lg bg-gray-200 hover:bg-gray-200"></div>
+      <div className="m-4 p-4 w-3/12 h-3/12 rounded-lg bg-gray-200 hover:bg-gray-200"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Shimmy;
